98e01c8ae79b7cd1c6109cadc88b0391a4783e39
Elsborg <Martin.elsborg@gmail.com>
2024-06-06 21:00:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>express app using watchtower!</h1>')
+  res.send('<h1>Now this should be updated automatically</h1>')
 })
 
 const PORT = 8080
